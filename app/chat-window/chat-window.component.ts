529c99e6c33c3233d955487447a89d545843bc85
Hoang Nguyen <hoang.nguyenba@hotmail.com>
2016-06-08 14:56:46 +0700

    Make messages auto scroll

--- app/chat-window/chat-window.component.ts
+++ app/chat-window/chat-window.component.ts
@@ -47,13 +47,13 @@ export class ChatWindowComponent implements OnInit {
 
     this.currentUser = this.userService.getCurrentUser();
 
-    // this.messages
-    //   .subscribe(
-    //     (messages: Array<Message>) => {
-    //       setTimeout(() => {
-    //         this.scrollToBottom();
-    //       });
-    //     });
+    this.messages
+      .subscribe(
+        (messages: Array<Message>) => {
+          setTimeout(() => {
+            this.scrollToBottom();
+          });
+        });
   }
 
   onEnter(event: any): void {
@@ -70,10 +70,10 @@ export class ChatWindowComponent implements OnInit {
     this.draftMessage = new Message();
   }
 
-  // scrollToBottom(): void {
-  //   let scrollPane: any = this.el
-  //     .nativeElement.querySelector('.msg-container-base');
-  //   scrollPane.scrollTop = scrollPane.scrollHeight;
-  // }
+  scrollToBottom(): void {
+    let scrollPane: any = this.el
+      .nativeElement.querySelector('.msg-container-base');
+    scrollPane.scrollTop = scrollPane.scrollHeight;
+  }
 
 }
