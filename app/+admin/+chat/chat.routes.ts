bfc4d7d74fac9bf9ccd6f8b3dfffe06e61861f72
Hoang Nguyen <hoang.nguyenba@hotmail.com>
2016-07-06 16:23:33 +0700

    clean

--- app/+admin/+chat/chat.routes.ts
+++ app/+admin/+chat/chat.routes.ts
@@ -2,8 +2,6 @@ import { RouterConfig } from '@angular/router';
 
 import { ChatComponent } from './chat.component';
 
-import { AuthGuard } from '../../auth.guard';
-
 export const ChatRoutes: RouterConfig = [
   {
     path: 'chat/:id',
